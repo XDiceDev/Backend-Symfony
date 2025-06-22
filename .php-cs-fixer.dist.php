c46fcd7c1f542569afe48732f63acc2ec831a248
LiquidDevQ <72251079+XDiceDev@users.noreply.github.com>
2025-06-23 00:10:08 +1000

    chore: configure PHP CS Fixer with PSR-12 and custom rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,35 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(['src', 'tests'])
+    ->name('*.php')
+    ->exclude(['vendor', 'var', 'config'])
 ;
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRules([
-        '@Symfony' => true,
+        '@PSR12' => true,
+        '@PhpCsFixer' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'no_extra_blank_lines' => true,
+        'no_trailing_whitespace' => true,
+        'line_ending' => true,
+        'single_quote' => true,
+        'phpdoc_ignore_psalm_tags' => true,
+        'strict_types_declarations' => true,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_functions' => true,
+        ],
     ])
+    ->setLineEnding("\n")
     ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setCacheFile('var/.php-cs-fixer.cache')
 ;
