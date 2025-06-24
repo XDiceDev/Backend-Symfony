839046b2ceb17f28720e37c896e77d9188678dfa
LiquidDevQ <72251079+XDiceDev@users.noreply.github.com>
2025-06-24 20:44:11 +1000

    Add create admin user command

---
+++ src/Command/CreateAdminUserCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-admin-user')]
+class CreateAdminUserCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Creates a new admin user')
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the admin user')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the admin user');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('Admin user created successfully!');
+
+        return Command::SUCCESS;
+    }
+}
